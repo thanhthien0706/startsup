f0dbd873544530365e92d479107b75f15aa791c6
Thanh Thien <thanhthien07062002@gmail.com>
2021-09-07 09:33:41 +0700

    bt

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+$(document).ready(function () {
+  $('.box_main_slide').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    arrows: false,
+    autoplay: true
+  });
+});
